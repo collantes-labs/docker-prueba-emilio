9ad1d392bfc00d56585203e3dc5ae0f07403b8b5
meltingghost <emiliodavidcc@gmail.com>
2023-12-21 14:23:47 -0400

    try to add network to mysql container

--- docker-postgre/docker-entrypoint.sh
+++ docker-postgre/docker-entrypoint.sh
@@ -2,3 +2,5 @@
 
 psql -U postgres -d postgresdb -c "CREATE USER emiliouser WITH PASSWORD '12345678';"
 psql -U postgres -d postgresdb -c "GRANT ALL PRIVILEGES ON DATABASE postgresdb TO emiliouser;"
+
+exec "$@"
